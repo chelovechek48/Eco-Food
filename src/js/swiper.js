a0f7a029360e1d667ea65554481de798e1111e49
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2023-12-16 13:42:10 +0300

    Added catalog CSS

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,19 @@
-// import Swiper from 'swiper';
-// import { Keyboard } from 'swiper/modules';
+import Swiper from 'swiper';
+import { Keyboard, Autoplay } from 'swiper/modules';
 
-// import '../styles/plugins/swiper.scss';
+import '../styles/plugins/swiper.scss';
 
-// const swiper = new Swiper('.swiper', {
-
-// });
+const swiper = new Swiper('.swiper', {
+  modules: [Keyboard, Autoplay],
+  keyboard: {
+    enabled: true,
+  },
+  autoplay: {
+    enabled: true,
+    delay: 3000,
+  },
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 48,
+  grabCursor: true,
+});
